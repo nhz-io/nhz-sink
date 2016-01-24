dc175e8758bf0242069009c2d468b44b37133163
Ishi Ruy <dev@nhz.io>
2016-01-24 03:59:39 +0200

    SINK-2 #comment better preprocessor instead of babel-jest

--- preprocessor.js
+++ preprocessor.js
@@ -1,10 +1,25 @@
 require('babel-register');
-var babelJest = require('babel-jest');
 var webpackAlias = require('jest-webpack-alias');
+var pkg = require('./package.json');
+var babel = require('babel-core');
+var presets = pkg.babel && pkg.babel.presets ? pkg.babel.presets : ['es2015'];
+
+babelPreprocessor = function(src, filename) {
+  if (babel.util.canCompile(filename)) {
+    return babel.transform(src, {
+      filename: filename,
+      retainLines: true,
+      presets: presets
+    }).code;
+  }
+
+  return src;
+}
+
 module.exports = {
   process: function(src, filename) {
     if (filename.indexOf('node_modules') === -1) {
-      src = babelJest.process(src, filename);
+      src = babelPreprocessor(src, filename);
       src = webpackAlias.process(src, filename);
     }
     return src;
