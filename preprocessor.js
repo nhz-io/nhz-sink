120ed84ca83799bfb5a4e1e2348e9242bb4e3b9c
Ishi Ruy <dev@nhz.io>
2016-01-24 05:24:59 +0200

    SINK-2 #comment added missing var

--- preprocessor.js
+++ preprocessor.js
@@ -4,7 +4,7 @@ var pkg = require('./package.json');
 var babel = require('babel-core');
 var presets = pkg.babel && pkg.babel.presets ? pkg.babel.presets : ['es2015'];
 
-babelPreprocessor = function(src, filename) {
+var babelPreprocessor = function(src, filename) {
   if (babel.util.canCompile(filename)) {
     return babel.transform(src, {
       filename: filename,
